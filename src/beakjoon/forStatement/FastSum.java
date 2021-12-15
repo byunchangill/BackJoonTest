21ac8db7c2b5c394f35d6a9ff05478f747953786
chang_ill <bchi2000@gmail.com>
2021-12-15 17:02:59 +0900

    21-12-15

--- src/beakjoon/forStatement/FastSum.java
+++ src/beakjoon/forStatement/FastSum.java
@@ -1,5 +1,23 @@
 package beakjoon.forStatement;
 
-public class FastSum {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
+public class FastSum {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int i = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int j = 0; j < i; j++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
